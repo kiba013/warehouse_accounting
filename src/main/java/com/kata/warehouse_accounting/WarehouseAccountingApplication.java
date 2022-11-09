ee51d66b8cbd264dc201b4bd453fca9a3081b93e
Sidorenko Maksim <sir.sidorenkomv@yandex.ru>
2022-11-09 10:20:29 +0300

    init

---
+++ src/main/java/com/kata/warehouse_accounting/WarehouseAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.kata.warehouse_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseAccountingApplication.class, args);
+    }
+
+}
